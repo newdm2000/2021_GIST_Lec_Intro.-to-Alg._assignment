90e10e5becd552875ceb9823437f5ab7ebad8c6c
Dongmin Shin <newdm2000@gmail.com>
2021-12-06 23:58:34 +0900

    2021-12-06 23:58 commit

--- assignment2/s20195091_2.py
+++ assignment2/s20195091_2.py
@@ -1,2 +1,13 @@
 def max_profit(cost):
-    
+    min = 10000
+    profit = 0
+    maxProfit = 0
+    for i in range(1,len(cost)):
+        if min > cost[i-1]: min = cost[i-1]
+        profit = max(0, cost[i]-min)
+        maxProfit = max(profit, maxProfit)
+    return maxProfit
+
+cost = [14,2,10,6,12,8]
+
+print(max_profit(cost))
