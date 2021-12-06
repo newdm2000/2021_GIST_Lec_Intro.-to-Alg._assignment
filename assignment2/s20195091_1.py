91856bc8ae7d872d875ccd6cc414b10cb8830a43
dongmin Shin <newdm2000@gmail.com>
2021-12-07 01:53:31 +0900

    2021-12-07 01:53 commit

--- assignment2/s20195091_1.py
+++ assignment2/s20195091_1.py
@@ -1,34 +1,24 @@
-from typing import List
 import heapq
 
-lists = [[1,4,5],[1,3,4],[2,6]]
-
 def sorted_list(lists):
-    root = 
-
-class ListNode:
-   def __init__(self, x):
-        self.val = x
-        self.next = None
-
-class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        root = result = ListNode(None)
-        heap = []
-
-        for i in range(len(lists)):
-            if lists[i]:
-                heapq.heappush(heap, (lists[i].val, i, lists[i]))
-
-        while heap:
-            node = heapq.heappop(heap)
-            idx = node[1]
-            result.next = node[2]
-
-            result = result.next
-            if result.next:
-                heapq.heappush(heap, (result.next.val, idx, result.next))
-
-        return root.next
+    K = len(lists)
+    s_list = []
+    heap = []
     
-Solution().mergeKLists(lists)
+    for i in range(0, K):
+        heapq.heappush(heap, (lists[i][0], i))
+    
+    while len(heap)>0:
+        j = heapq.heappop(heap)
+        q = j[1]
+        a = lists[q].pop(0)
+        s_list.append(a)
+        if len(lists[q]) != 0:
+            heapq.heappush(heap, (lists[q][0], q))
+        else:
+            
+            pass
+    
+    return s_list
+    
+sorted_list([])
