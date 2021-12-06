d747e0c995108d0b0e937e51129d5b2bec6e0555
Dongmin Shin <newdm2000@gmail.com>
2021-12-06 23:58:13 +0900

    2021-12-06 23:58 commit

--- assignment2/s20195091_1.py
+++ assignment2/s20195091_1.py
@@ -1,35 +1,34 @@
+from typing import List
+import heapq
+
+lists = [[1,4,5],[1,3,4],[2,6]]
+
+def sorted_list(lists):
+    root = 
+
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        
+   def __init__(self, x):
+        self.val = x
+        self.next = None
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        data = []
-        if len(lists) == 0: return None
-            
-        if len(lists) == 1: return lists[0]
-            
-        for lst in lists:
-            if lst == None: continue
-                
-            data.append(lst.val)
-            while lst.next: # 노드가 끝날 때까지 탐색
-                lst = lst.next
-                data.append(lst.val)
+        root = result = ListNode(None)
+        heap = []
 
-        lists = ListNode()
-        if len(data) == 0: return None
-        data.sort()
-        
-        for i in range(len(data)):
-            if i == 0:
-                lists.val = data[i]
-            else:
-                node = lists
-                # node.next가 None 없을 때까지, 더 input할 엘리먼트가 없을 때까지
-                while node.next: 
-                    node = node.next
-                node.next = ListNode(data[i])
-                
-        return lists
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next
+    
+Solution().mergeKLists(lists)
